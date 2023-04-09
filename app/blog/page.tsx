feeeda476d932e46c92b4c3a448ddc432178a652
Trevor Blades <tdblades@gmail.com>
2023-04-09 16:10:24 -0700

    Get the page metadata in the list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,14 +5,26 @@ import fs from "fs/promises";
 export default async function BlogPage() {
   const posts = await fs
     .readdir("app/blog/(posts)", { withFileTypes: true })
-    .then((files) => files.filter((file) => file.isDirectory()));
+    .then((files) => files.filter((file) => file.isDirectory()))
+    .then(async (files) =>
+      Promise.all(
+        files.map(async (file) => {
+          const { metadata } = await import(`./(posts)/${file.name}/page.mdx`);
+          return {
+            slug: file.name,
+            metadata,
+          };
+        })
+      )
+    );
+
   return (
     <div>
       Blog home
       <ul>
         {posts.map((post, index) => (
           <li key={index}>
-            <Link href={`/blog/${post.name}`}>{post.name}</Link>
+            <Link href={`/blog/${post.slug}`}>{post.metadata.title}</Link>
           </li>
         ))}
       </ul>
