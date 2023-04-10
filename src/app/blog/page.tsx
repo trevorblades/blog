b165ace5ca16ea932908639398012d366eba48cd
Trevor Blades <tdblades@gmail.com>
2023-04-10 00:50:18 -0700

    Move everything within src

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+import { readdir } from "fs/promises";
+
+export default async function BlogPage() {
+  const nodes = await readdir("app/blog/(posts)", { withFileTypes: true });
+  const files = nodes.filter((node) => node.isDirectory());
+  const posts = await Promise.all(
+    files.map(async (file) => {
+      const { metadata } = await import(`./(posts)/${file.name}/page.mdx`);
+      return {
+        slug: file.name,
+        metadata,
+      };
+    })
+  );
+
+  return (
+    <div>
+      <h1 className="text-lg">Blog home</h1>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <Link href={`/blog/${post.slug}`}>{post.metadata.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
+// @ts-check
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
